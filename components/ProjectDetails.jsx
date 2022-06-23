90859a37e85d9ce437b4aa13bee30542374be636
polthemarketer <psocarde@gmail.com>
2022-06-23 23:22:14 +0300

    Update ProjectDetails.jsx

--- components/ProjectDetails.jsx
+++ components/ProjectDetails.jsx
@@ -1,6 +1,11 @@
 import React from 'react'
 
-import { Container, Text, SimpleGrid, Box, Center, VStack } from '@chakra-ui/react';
+import { Image, Text, SimpleGrid, Box, Center, VStack, HStack } from '@chakra-ui/react';
+
+import Twitter from '../assets/icons/twitter.png'
+import Discord from '../assets/icons/discord.png'
+import Website from '../assets/icons/click.png'
+
 
 const ProjectDetails = () => {
   return (
@@ -23,6 +28,23 @@ const ProjectDetails = () => {
                         </VStack>
                     </Box>
             </SimpleGrid>
+               <Center>
+                <VStack mt={20} gap='5'>
+                <Text align='center' fontSize={40} color='white'><b>About us...</b></Text>
+                    <Text mb='10' maxW={'50%'} noOfLines={4} color='white' align='center'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</Text>
+            <Box bgColor={"#FF0080"} px='6' py='2' borderRadius='40'>
+                <HStack >
+                    <Text color='white'>Follow us on Social Media:</Text>
+                        <HStack>
+                            <a href='#' target='_blank'><Image src={Twitter.src} alt='Twitter' w={5}/></a>
+                            <a href='#' target='_blank'><Image src={Discord.src} alt='Discord' w={5}/></a>
+                            <a href='#' target='_blank'><Image src={Website.src} alt='Website' w={5}/></a>
+                        </HStack>
+                        
+                 </HStack>
+                 </Box>
+                 </VStack>
+                </Center> 
            
     </>
   )
