52cce45eecf590e238210ba555335a1c9458b8c7
polthemarketer <psocarde@gmail.com>
2022-07-05 19:30:42 +0300

    Update Farming.jsx

--- pages/Farming.jsx
+++ pages/Farming.jsx
@@ -1,10 +1,30 @@
 import React from 'react'
 
-import { Input, Button, Text, Grid, GridItem, Box, Center, VStack, HStack, Container, Flex } from '@chakra-ui/react';
+import { Input, Button, Text, Grid, GridItem, Box, Center, VStack, HStack, Container, 
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalFooter,
+  ModalBody,
+  ModalCloseButton, useDisclosure, InputLeftAddon, InputGroup } from '@chakra-ui/react';
 
 import Background from '../assets/bg-tabs.png';
 
 const Farming = () => {
+  const OverlayTwo = () => (
+    <ModalOverlay
+      // bg='black'
+      // opacity='0.2'
+      backdropFilter='auto'
+      // backdropInvert='10%'
+      backdropBlur='2px'
+    />
+  )
+
+  const { isOpen, onOpen, onClose } = useDisclosure()
+  const [overlay, setOverlay] = React.useState(<OverlayTwo />)
+
   return (
     <>
     <Container maxW={'100%'}>
@@ -141,7 +161,11 @@ const Farming = () => {
                         maxW={'20%'}
                         fontSize={['12px', null, null, null, '100%']}
                         _hover={{bgGradient: "linear(to-l, #8a32e3, #FF0080)", color: "white"}}
-                        px={15} borderRadius={40}>
+                        px={15} borderRadius={40}
+                        onClick={() => {
+                          setOverlay(<OverlayTwo />)
+                          onOpen()
+                        }}>
                       <b>+</b>
                       </Button>
                         </HStack>
@@ -170,6 +194,38 @@ const Farming = () => {
                     </Box>
     </Center>
     </Container>
+
+    <Modal isCentered isOpen={isOpen} onClose={onClose}>
+        {overlay}
+        <ModalContent>
+          <ModalHeader>Farm DVX-BNB LP</ModalHeader>
+          <ModalCloseButton />
+          <ModalBody>
+            <Text><b>DVX-BNB LP in your wallet:</b> 23,456</Text>
+            <Text><b>DVX-BNB LP farmed:</b> 23,456</Text>
+            <br />
+            <InputGroup>
+              <InputLeftAddon children='DVX-BNB LP' />
+              <Input type='number' placeholder='Amount To Farm' />
+            </InputGroup>
+          </ModalBody>
+          <ModalFooter>
+          <Button
+              variant={'solid'}
+              size='sm'
+              bgGradient='linear(to-l, #7928CA, #FF0080)'
+              color='white'
+              maxW={['75%', null, '100%', '100%', '100%']}
+              ml="30px"
+              fontSize={['12px', null, null, null, '100%']}
+              _hover={{bgGradient: "linear(to-l, #8a32e3, #FF0080)", color: "white"}}
+              display={{ base: 'none', md: 'flex' }} px={6} borderRadius={20}>
+             Star Farming
+            </Button>
+            {/* <Button onClick={onClose}>Close</Button> */}
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
     </>
   )
 }
