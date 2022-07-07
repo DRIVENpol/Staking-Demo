65fb8460119425c6ae201ca1b5075cd59919743e
polthemarketer <psocarde@gmail.com>
2022-07-07 12:41:31 +0300

    Update Staking.jsx

--- pages/Staking.jsx
+++ pages/Staking.jsx
@@ -1,13 +1,72 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 
 
 import { Text, Center, Container } from '@chakra-ui/react';
 import { motion, useAnimation } from 'framer-motion';
 
-import StakingBox from '../components/StakingBox';
+import { ethers } from "ethers";
+
+
+import FarmingBox from '../components/FarmingBox';
 import AnalyticsFarming from '../components/AnalyticsFarming';
 
 const Staking = () => {
+
+  const mainScAddress = "0x5F787db64B1313B981579A02673559f292f552DB";
+
+  const tokenPrice = 2.476;
+
+  const [poolDetails, setPollDetails] = useState({
+    lockedTokens: 0,
+    activeFarmers: 0,
+    givenRewards: 0,
+    owner: ''
+   });
+
+   const getPoolDetails = async () => {
+    const iProvider = new ethers.providers.JsonRpcProvider("https://eth-rinkeby.alchemyapi.io/v2/qSQowMkVnht5prnNyhu9DF8w-oBdrcww");
+
+    const abi = ["function getOwner() public view returns(address)",
+    "function getActiveStakers() public view returns(uint256)",
+    "function getTotalTokensStaked() public view returns(uint256)",
+    "function getTotalGivenRewards() public view returns(uint256)"
+    ];
+
+    const connectedContract = new ethers.Contract(mainScAddress, abi, iProvider);
+
+    let _owner = await connectedContract.getOwner();
+    let _stakers = await connectedContract.getActiveStakers();
+    let _stakedTokens = await connectedContract.getTotalTokensStaked();
+    let _givenRewards = await connectedContract.getTotalGivenRewards();
+
+    let _tvlConverted = Number(_stakedTokens);
+    let _tvlWithoutDecimals = _tvlConverted / 10 ** 18;
+
+    let _givenRConverted = Number(_givenRewards);
+    let _givenRWithoutDecimals = _givenRConverted / 10 ** 18;
+
+    setPollDetails({
+      lockedTokens: (_tvlWithoutDecimals * tokenPrice).toLocaleString(),
+      activeFarmers: Number(_stakers),
+      givenRewards: _givenRWithoutDecimals.toLocaleString(),
+      owner: _owner
+      })
+
+      // Logs
+      console.log("======= PROJECT DETAILS ========");
+      console.log("Owner: " + _owner);
+      console.log("Locked Tokens: " + _stakedTokens / 10 ** 18);
+      console.log("Stakers: " + _stakers);
+      console.log("Rewards: " + _givenRewards);
+      console.log("===============");
+};
+
+useEffect(() => {
+  getPoolDetails();
+}, [])
+
+
+
 
   return (
     <>
@@ -18,10 +77,15 @@ const Staking = () => {
       </Text>
     </Center>
 
-   <AnalyticsFarming />
+   <AnalyticsFarming 
+    stakers = {poolDetails.activeFarmers}
+    lockedTokens = {poolDetails.lockedTokens}
+    givenRewards = {poolDetails.givenRewards}
+    owner = {poolDetails.owner}
+   />
 
       <Center>
-      <StakingBox />
+      <FarmingBox />
       </Center>
 
     </Container>
